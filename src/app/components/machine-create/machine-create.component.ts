05afd912b09fb1c97746d2b1ec9886c99b834577
LSaladino <luis.saladino@gmail.com>
2025-04-22 16:48:09 -0300

    adjust final

--- src/app/components/machine-create/machine-create.component.ts
+++ src/app/components/machine-create/machine-create.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
+
+
 @Component({
   selector: 'app-machine-create',
   templateUrl: './machine-create.component.html',
@@ -12,12 +14,12 @@ export class MachineCreateComponent implements OnInit {
 
   machineFleetCreateForm: FormGroup;
 
-  ngOnInit(): void {
+  ngOnInit()  {
     this.machineFleetCreateForm = new FormGroup({
-      id: new FormControl(['', Validators.required]),
-      name: new FormControl(['', Validators.required]),
-      status: new FormControl([0, Validators.required]),
-      performance: new FormControl([0, Validators.required]),
+      id: new FormControl(null, Validators.required),
+      name: new FormControl(null, [Validators.required, Validators.pattern(/^[a-zA-Z0-9 ]*$/)]),
+      status: new FormControl(null, Validators.required),
+      performance: new FormControl(null, Validators.required),
 
       performanceLog: new FormArray([]), //###############################
 
@@ -25,44 +27,70 @@ export class MachineCreateComponent implements OnInit {
 
       errorHistory: new FormArray([]), //###############################
 
-      producedParts: new FormControl([0, Validators.required]),
+      producedParts: new FormControl(null, Validators.required),
       production: new FormGroup({
-        currentProduct: new FormControl(['', Validators.required]),
-        partsInBatch: new FormControl([0, Validators.required]),
-        partsCompleted: new FormControl([0, Validators.required]),
-        batchStartTime: new FormControl(['', Validators.required])
+        currentProduct: new FormControl(null, Validators.required),
+        partsInBatch: new FormControl(null, Validators.required),
+        partsCompleted: new FormControl(null, Validators.required),
+        batchStartTime: new FormControl(null, Validators.required)
       }),
       details: new FormGroup({
-        manufacturer: new FormControl(['', Validators.required]),
-        modelNumber: new FormControl(['', Validators.required]),
-        lastMaintenance: new FormControl(['', Validators.required]),
-        location: new FormControl(['', Validators.required])
+        manufacturer: new FormControl(null, Validators.required),
+        modelNumber: new FormControl(null, Validators.required),
+        lastMaintenance: new FormControl(null, Validators.required),
+        location: new FormControl(null, Validators.required)
       }),
 
       maintenanceHistory: new FormArray([]), //###############################
 
       liveSensors: new FormGroup({
-        temperatureC: new FormControl([0, Validators.required]),
-        vibrationLevel: new FormControl([0, Validators.required]),
-        powerConsumptionKw: new FormControl([0, Validators.required])
+        temperatureC: new FormControl(null, Validators.required),
+        vibrationLevel: new FormControl(null, Validators.required),
+        powerConsumptionKw: new FormControl(null, Validators.required)
       }),
       currentShift: new FormGroup({
-        operatorName: new FormControl(['', Validators.required]),
-        shift: new FormControl(['', Validators.required])
+        operatorName: new FormControl(null, Validators.required),
+        shift: new FormControl(null, Validators.required)
       })
     });
 
-    this.machineFleetCreateForm.valueChanges.subscribe((value) => {
-      this.formData = value;
-      console.log('ValueChanges ->', this.formData);
-    });
+    // this.machineFleetCreateForm.valueChanges.subscribe((value) => {
+    //   this.formData = value;
+    //   console.log('ValueChanges ->', this.formData);
+    // });
 
-    this.machineFleetCreateForm.statusChanges.subscribe((status) => {
-      this.formStatus = status;
-      console.log('StatusChanges ->', this.formStatus);
-    });
+    // this.machineFleetCreateForm.statusChanges.subscribe((status) => {
+    //   this.formStatus = status;
+    //   console.log('StatusChanges ->', this.formStatus);
+    // });
 
   }
+
+  onSubmit() {
+    console.log('Form Submitted', this.machineFleetCreateForm.value);
+    if (this.machineFleetCreateForm.valid) {
+      console.log('Form Submitted', this.machineFleetCreateForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+
+  }
+
+  onCloseForm() {}
+
+  onResetForm() {
+    this.machineFleetCreateForm.reset();
+    console.log('Form Reset');
+  }
+
+
+
+
+
+
+
+
+
 
 
 
